ba268e8f19b1b88313878f310019e35d71f29df2
rajivnadendla <68058338+rajivnadendla@users.noreply.github.com>
2020-07-12 17:23:24 +0530

    Initial commit

---
+++ dotnet3.1-in-docker/Controllers/AppController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet3._1_in_docker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppController : ControllerBase
+    {
+
+        private readonly ILogger<AppController> _logger;
+
+        public AppController(ILogger<AppController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
